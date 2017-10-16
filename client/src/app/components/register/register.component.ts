82645e37d6cd46bd026dfe0c70bf654f914c0e6f
Atmiyata <jayraj.solanki50@gmail.com>
2017-10-16 11:54:48 -0400

    register component => service response added to front-end

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
-import {RegisterService} from '../../services/register.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from '../../services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -10,74 +10,82 @@ import {RegisterService} from '../../services/register.service';
 
 export class RegisterComponent implements OnInit {
 
-  public form :FormGroup;
-  public user:any=[];
-  public passwordNotMatch:boolean;
+  public form: FormGroup;
+  public user: any = [];
+  public passwordNotMatch: boolean;
+  public message: string;
+  public messageClass: string;
 
-   registerUser = {
-     username:'',
-     email:'',
-     password:''
-   };
-      
+  registerUser = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
   constructor(
-    private formBuilder:FormBuilder,
-    private registerService:RegisterService) { 
+    private formBuilder: FormBuilder,
+    private registerService: RegisterService) {
     this.createForm();
   }
 
   ngOnInit() {
   }
 
-  public createForm(){
+  public createForm() {
     this.form = this.formBuilder.group({
-      username:['',Validators.compose([
+      username: ['', Validators.compose([
         Validators.required,
         Validators.minLength(3),
         Validators.maxLength(20)
       ])],
-      email:['',Validators.compose([
+      email: ['', Validators.compose([
         Validators.required,
         Validators.minLength(5),
         Validators.maxLength(40),
         this.validateEmail
       ])],
-      password:['',Validators.compose([
+      password: ['', Validators.compose([
         Validators.required,
         Validators.minLength(6),
         Validators.maxLength(20)
       ])],
-      confirm:['',Validators.required]
+      confirm: ['', Validators.required]
     });
   }
 
-  public validateEmail(controls){
+  public validateEmail(controls) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(re.test(controls.value)){
+    if (re.test(controls.value)) {
       return null;
-    }else{
-      return {'validateEmail':true}
+    } else {
+      return { 'validateEmail': true }
     }
   }
 
-  public matchingPasswords(){
-    if(this.user.password !== this.user.confirm){
-       this.passwordNotMatch = true;
-     }else{
-       this.passwordNotMatch = false;
-     }
+  public matchingPasswords() {
+    if (this.user.password !== this.user.confirm) {
+      this.passwordNotMatch = true;
+    } else {
+      this.passwordNotMatch = false;
+    }
   }
 
-  public submitForm(){
+  public submitForm() {
     this.matchingPasswords();
     this.registerUser.username = this.user.username;
     this.registerUser.email = this.user.email;
     this.registerUser.password = this.user.password;
-    if(this.passwordNotMatch == false){
-      
-      this.registerService.registerUser(this.registerUser).subscribe(res =>{
-           console.log(res);
- });
+    if (this.passwordNotMatch == false) {
+
+      this.registerService.registerUser(this.registerUser).subscribe(res => {
+        this.message = res.message;
+        if (!res.success) {
+          this.messageClass = "alert alert-danger";
+        } else {
+          this.messageClass = "alert alert-success";
+        }
+
+      });
     }
   }
 }
