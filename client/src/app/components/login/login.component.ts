366ee5351120d0ec65eb22e360dcec27e5371de8
Atmiyata <jayraj.solanki50@gmail.com>
2017-10-17 11:25:41 -0400

    Login link with back-end

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -8,18 +8,41 @@ import { LoginService } from '../../services/login.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
-public form: FormGroup;
-public user:any = [];
-  constructor( private formBuilder: FormBuilder,
-    private loginService:LoginService) { }
+  public form: FormGroup;
+  public message;
+  public messageClass;
+  public user: any = [];
+
+  private userLogin = {
+    "username": '',
+    "password": ''
+  };
+  constructor(private formBuilder: FormBuilder,
+    private loginService: LoginService) {
+    this.createForm();
+  }
 
   ngOnInit() {
   }
- 
-  loginUser() {
-    this.loginService.loginUser(this.user).subscribe(data =>{
-       
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    this.userLogin.username = this.user.username;
+    this.userLogin.password = this.user.password;
+    this.loginUser();
+  }
+
+  private loginUser() {
+    this.loginService.loginUser(this.userLogin).subscribe(data => {
+      console.log(data);
     });
   }
 }
